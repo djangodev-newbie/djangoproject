155b6ebef8264c7eab3e6194f3c74461f5abfd77
spiderxm <mrigank.rkee@rediffmail.com>
2020-10-29 15:36:44 +0530

    making my first api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def getData(response):
+    return JsonResponse({"hello" : "my first api"})
