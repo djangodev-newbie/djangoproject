551f739c959e6e43d1e361c5985f4237d7f28fed
spiderxm <mrigank.rkee@rediffmail.com>
2020-10-29 15:44:25 +0530

    making my first post request

--- api/views.py
+++ api/views.py
@@ -2,8 +2,11 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
 
-
-def getData(response):
+@csrf_exempt
+def getData(request):
+    if request.method == "POST":
+        return JsonResponse({"hello": "Post request placed"})
     return JsonResponse({"hello" : "my first api"})
